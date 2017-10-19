9fe374aaa8081128e0db083092ce17f96f7bf3fb
bclittleua <32556428+bclittleua@users.noreply.github.com>
2017-10-19 16:00:33 -0700

    Add files via upload

---
+++ temp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+( 
+cpuTemp0=$(cat /sys/class/thermal/thermal_zone0/temp)
+cpuTemp1=$(($cpuTemp0/1000))
+cpuTemp2=$(($cpuTemp0/100))
+cpuTempM=$(($cpuTemp2 % $cpuTemp1))
+date 
+echo $cpuTemp1.$cpuTempM
+echo $cpuTemp1.$cpuTempM *1.8+32 | bc
+) &>> /home/pi/templog
