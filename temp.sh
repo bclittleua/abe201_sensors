19c3f0bb56166a811b7a984afa1558944f3e7f62
bclittleua <32556428+bclittleua@users.noreply.github.com>
2017-10-19 16:06:19 -0700

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-( 
+(
 cpuTemp0=$(cat /sys/class/thermal/thermal_zone0/temp)
 cpuTemp1=$(($cpuTemp0/1000))
 cpuTemp2=$(($cpuTemp0/100))
 cpuTempM=$(($cpuTemp2 % $cpuTemp1))
-date 
+date
 echo $cpuTemp1.$cpuTempM
-echo $cpuTemp1.$cpuTempM *1.8+32 | bc
-) &>> /home/pi/templog
+echo $cpuTemp1.$cpuTempM * 1.8 + 32 | bc
+) &>> templog
