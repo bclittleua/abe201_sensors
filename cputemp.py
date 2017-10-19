0106c8d9bb57c9a70718180b54b37566c1b5cfce
bclittleua <32556428+bclittleua@users.noreply.github.com>
2017-10-19 15:57:01 -0700

    Update cputemp.py

--- cputemp.py
+++ cputemp.py
@@ -5,7 +5,7 @@ cpu = CPUTemperature()
 
 def write_temp(temp):
 	with open("cputemp", "a") as log:
-		log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%<:%S"),str(temp)))
+		log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp)))
 		
 while True:
 	temp = cpu.temperature
