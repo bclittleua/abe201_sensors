610c5a4dff9cf93caf9b34f1e433044a35af0f76
bclittleua <32556428+bclittleua@users.noreply.github.com>
2017-10-20 19:16:55 -0700

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 (
-cpuTemp0=$(cat /sys/class/thermal/thermal_zone0/temp)
+cpuTemp0=$(cat /sys/thermal/thermal_zone0/temp)
 cpuTemp1=$(($cpuTemp0/1000))
 cpuTemp2=$(($cpuTemp0/100))
-cpuTempM=$(($cpuTemp2 % $cpuTemp1))
+cpuTempM=$(($cpuTemp2 % cpuTemp1))
 date
 echo $cpuTemp1.$cpuTempM
 echo $cpuTemp1.$cpuTempM *1.8+32 | bc
